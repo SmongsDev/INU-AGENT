d8f79a902b66cb0808ef4fad2339cd9cab7a9601
insonggim <insonggim8@gmail.com>
2025-08-02 07:00:09 +0900

    오탐 Agent Input state 변경 및 오류 수정

--- langgraph_flow/nodes/summarize.py
+++ langgraph_flow/nodes/summarize.py
@@ -1,13 +1,14 @@
 from typing import TypedDict
-from langgraph_flow.nodes.rag.document_converter import convert_event_to_text
+from app.schemas.cloudtrail import CloudTrailEvent
+from langgraph_flow.nodes.rag.document_converter import convert_cloudtrail_to_text
 
 class State(TypedDict):
-    event: dict
+    event: CloudTrailEvent
     event_summary: str
 
 def summarize_event(state: State) -> State:
     """이벤트를 요약하여 텍스트로 변환합니다."""
-    event_summary = convert_event_to_text(state['event'])
+    event_summary = convert_cloudtrail_to_text(state['event'])
     return {
         'event_summary': event_summary,
     } 
