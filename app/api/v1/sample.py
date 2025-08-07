bf3c705064a1ad92d3ed12e95758a9f5d80d7993
insonggim <insonggim8@gmail.com>
2025-08-07 15:40:26 +0900

    git action test v8

--- app/api/v1/sample.py
+++ app/api/v1/sample.py
@@ -5,4 +5,4 @@ router = APIRouter()
 
 @router.post("/sample")
 async def process_tier1_data(tier1_data: Tier1) -> str:
-    return f"처리된 Data: IP {tier1_data.source_ip}에서 {tier1_data.event_type} 이벤트 발생, 에러 코드: {tier1_data.error_code or '없음'}"
+    return f"처리된 Data: IP {tier1_data.source_ip}에서 {tier1_data.event_type} 이벤트 발생, error_code: {tier1_data.error_code or '없음'}"
