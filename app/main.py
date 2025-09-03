058d9a06ad71a61d2956cae8af9029efaedf5744
insonggim <insonggim8@gmail.com>
2025-09-03 10:41:07 +0900

    cors 수정

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app = FastAPI(lifespan=lifespan)
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
