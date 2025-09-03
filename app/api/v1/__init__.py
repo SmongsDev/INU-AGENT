ef59d7d1a8f44112e9f67c2d212220b699d3c5b7
SmongsDev <sssm0928@naver.com>
2025-09-03 16:18:00 +0900

    feat: routing init 수정

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from . import routes_group
 from . import routes_user
+from . import routes_agentflow
+from . import routes_dashboard
 
 router = APIRouter()
 
 router.include_router(routes_user.router, tags=["Users"])
 router.include_router(routes_group.router, tags=["Groups"])
+router.include_router(routes_agentflow.router, tags=["AgentFlow"])
+router.include_router(routes_dashboard.router, tags=["DashBoard"])
